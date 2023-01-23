f10c4f9a8e14ab13d3135d478dd889aa33bd77bf
tiffenie28 <123345704+tiffenie28@users.noreply.github.com>
2023-01-23 15:08:52 -0600

    Homework1

---
+++ Homework1.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework1;
+class Equation
+{
+    static void Main(string[] args)
+    {
+        double X = 2.5;
+        double Y = 3.3;
+        double Z;
+          Z = 4*X*X+3*Y;
+          Console.WriteLine("X =" + " " + X + ", " + "Y =" + " " + Y);
+          Console.WriteLine("The value of Z is:" + " " + Z);
+    }
+}
