995c1b66c48512762b76d358ee231a8189bbb870
tiffenie28 <123345704+tiffenie28@users.noreply.github.com>
2023-01-29 21:47:06 -0600

    Add files via upload

---
+++ Homework2Q1.cs
@@ -0,0 +1,23 @@
+﻿namespace Lecture2;
+class Program
+{
+    static void Main(string[] args){
+     Console.WriteLine("Please input a letter grade:");
+
+        switch (Console.ReadLine()[0]) {
+       
+        case 'A': Console.WriteLine("GPA point: 4");
+                  break;
+        case 'B': Console.WriteLine("GPA point: 3");
+                  break;
+        case 'C': Console.WriteLine("GPA point: 2");
+                  break;
+        case 'D': Console. WriteLine("GPA point: 1");
+                  break;
+        case 'F': Console.WriteLine("GPA point: 0"); 
+                  break; 
+                default:  Console.WriteLine("Wrong Letter Grade!");
+                  break;
+    }
+  }
+}
